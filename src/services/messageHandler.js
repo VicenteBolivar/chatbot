f3e89f99d4ad110833536da9e27cbcdeb4787da6
Vicente <bolivar933@gmail.com>
2026-03-30 12:42:51 -0500

    se creo proyecto chatbot

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -1,15 +1,19 @@
 const whatsappService = require('./whatsappService');
 
 class MessageHandler {
+  constructor() {
+    this.processedMessages = new Set(); // 👈 AQUÍ ESTÁ LA CLAVE
+  }
+
   async handleIncomingMessage(message, senderInfo) {
     const messageId = message.id;
 
-    if (processedMessages.has(messageId)) {
+    if (this.processedMessages.has(messageId)) {
       console.log("⚠️ Mensaje duplicado ignorado:", messageId);
       return;
     }
 
-    processedMessages.add(messageId);
+    this.processedMessages.add(messageId);
 
     if (message?.type === 'text') {
       const incomingMessage = message.text.body.toLowerCase().trim();
@@ -18,7 +22,7 @@ class MessageHandler {
         await this.sendWelcomeMessage(message.from, message.id);
       } else {
         const response = "No entendí tu mensaje";
-        await whatsappService.sendMessage(message.from, response, message.id);
+        await whatsappService.sendMessage(message.from, response);
       }
 
       await whatsappService.markAsRead(message.id);
